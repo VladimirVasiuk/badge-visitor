ce57d385ba83d3c55f4d2d601e455ee6d0a5ab8e
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-01 19:52:46 -0700

    create CustomException
    Visitor not found by phone
    Visitor wasn't created
    Visitor cannot checkout

---
+++ src/main/java/com/galvanize/badgevisitor/exception/CustomExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.galvanize.badgevisitor.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(VisitorNotFoundException.class)
+    protected ResponseEntity<CustomException> handlePersonNotFoundException(VisitorNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(new CustomException(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getLocalizedMessage(),
+                request.getDescription(false)), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(VisitorNotCreatedException.class)
+    protected ResponseEntity<CustomException> handlePersonNotCreatedException(VisitorNotCreatedException ex, WebRequest request) {
+        return new ResponseEntity<>(new CustomException(
+                HttpStatus.EXPECTATION_FAILED.value(),
+                ex.getLocalizedMessage(),
+                request.getDescription(false)), HttpStatus.EXPECTATION_FAILED);
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class CustomException {
+        private int error_code;
+        private String message;
+        private String uri;
+    }
+}
+
